feb4e4e5b3ebce66e0510cdb9425a5515ab78ee3
Krystian Fras <krystian@fras.me>
2019-11-09 13:20:32 +0100

    added css

--- boilerplate/pages/admin/product.vue
+++ boilerplate/pages/admin/product.vue
@@ -1,29 +1,49 @@
 <template>
-  <div id="editor">
-    <textarea :value="input" @input="update" />
-    <div v-html="compiledMarkdown" />
-  </div>
+  <form @submit.prevent="saveProduct()">
+    <input v-model="product.name" class="dib" type="text" placeholder="name">
+    <input v-model="product.pictureUrl" class="dib" type="text" placeholder="url">
+    <div id="editor">
+      <textarea :value="product.description" @input="update" />
+      <div v-html="compiledMarkdown" />
+    </div>
+
+    <button type="submit">
+      Add product
+    </button>
+  </form>
 </template>
 <script>
 import marked from 'marked'
 
 export default {
   data: () => ({
-    input: '# hello'
+    product: {
+      name: '',
+      description: '',
+      pictureUrl: ''
+    }
   }),
   computed: {
     compiledMarkdown () {
-      return marked(this.input)
+      return marked(this.product.description)
     }
   },
   methods: {
     update (e) {
-      this.input = e.target.value
+      this.product.description = e.target.value
+    },
+
+    saveProduct () {
+      console.log(this.product)
     }
   }
 }
 </script>
 <style lang="css" scoped>
+.db {
+  display: block;
+}
+
 #editor {
   height: 100vh;
   margin: 0;
